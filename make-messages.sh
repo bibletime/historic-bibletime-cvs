1a2dcef0dceecabcfa6e57d1e551f1656cd916a8
joachim <joachim>
2001-03-18 14:13:23 +0000

    Initial revision

---
+++ make-messages.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+perl ./extractrc bibletime/xml/*.rc > bibletime/rc-dummy.cpp && \
+sed "s/#define//" bibletime/whatsthisdef.h > bibletime/dummy.h && \
+xgettext -C -x $KDEDIRS/include/kde.pot -ki18n -kI18N_NOOP -o po/bibletime.pot `find ./ -name \*.cpp` `find . -name \*.h` &&  \
+cd po && make merge && \
+rm ../bibletime/dummy.h && \
+rm ../bibletime/rc-dummy.cpp
+
