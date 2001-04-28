e1eb339988ab0b54e21f6dc302f258f7979c9b7f
saalbach_fk <saalbach_fk>
2001-04-28 20:32:44 +0000

    shell script to launch bibletime

--- bibletime/frontend/thirdparty/qt3stuff/libqt3stuff_a_meta_unload.cpp
+++ bibletime/frontend/thirdparty/qt3stuff/libqt3stuff_a_meta_unload.cpp
@@ -1,8 +1,8 @@
 static const char * _metalist_libqt3stuff_a[] = {
-"QTextEdit",
 "Qt3::QStyleSheet",
 "Qt3::QTextDocument",
 "Qt3::QTextView",
+"QTextEdit",
 0};
 #include <kunload.h>
 _UNLOAD(libqt3stuff_a)
