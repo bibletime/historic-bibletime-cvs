5f34644fd8e6ac6c4d6169bad29a58feb19f0550
mgruner <mgruner>
2001-11-19 20:54:39 +0000

    *** empty log message ***

---
+++ bibletime/frontend/optionsdialog/cbtconfig.cpp
@@ -0,0 +1,23 @@
+/***************************************************************************
+                          cbtconfig.cpp  -  description
+                             -------------------
+    begin                : Mon Nov 19 2001
+    copyright            : (C) 2001 by The BibleTime team
+    email                : info@bibletime.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "cbtconfig.h"
+
+CBTConfig::CBTConfig(){
+}
+CBTConfig::~CBTConfig(){
+}
---
+++ bibletime/frontend/optionsdialog/cbtconfig.h
@@ -0,0 +1,32 @@
+/***************************************************************************
+                          cbtconfig.h  -  description
+                             -------------------
+    begin                : Mon Nov 19 2001
+    copyright            : (C) 2001 by The BibleTime team
+    email                : info@bibletime.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef CBTCONFIG_H
+#define CBTCONFIG_H
+
+
+/**This class is the interface to the config object of BibleTime
+  *@author The BibleTime team
+  */
+
+class CBTConfig {
+public: 
+	CBTConfig();
+	~CBTConfig();
+};
+
+#endif
