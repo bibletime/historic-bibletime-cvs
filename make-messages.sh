166adcb03c36e73c51ce4485555271274b6ad6e2
mgruner <mgruner>
2002-04-04 20:25:13 +0000

    fit to Martin's system

--- make-messages.sh
+++ make-messages.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-myDir=/opt/kde2
+myDir=$KDEDIR/include/kde
 target=../bibletime-doc
 
 current=$PWD
@@ -8,9 +8,11 @@ perl ./extractrc bibletime/xml/*.rc > bibletime/rc-dummy.cpp && \
 sed "s/#define//" bibletime/tooltipdef.h > bibletime/dummy.h && \
 sed "s/#define//" bibletime/whatsthisdef.h >> bibletime/dummy.h && \
 perl ./preparetips $target/docs/en/tips > bibletime/tips-dummy.cpp && \
-xgettext -C -x $myDir/include/kde.pot -ki18n -kI18N_NOOP -o $target/po/bibletime.pot `find ./ -name \*.cpp` `find . -name \*.h` &&  \
+xgettext -C -x $myDir/kde.pot -ki18n -kI18N_NOOP -o $target/po/bibletime.pot `find ./ -name \*.cpp` `find . -name \*.h` &&  \
 cd $target/po && make merge && rm $current/bibletime/dummy.h && rm $current/bibletime/rc-dummy.cpp && rm $current/bibletime/tips-dummy.cpp
 
 
 cd $current
+
+
 
