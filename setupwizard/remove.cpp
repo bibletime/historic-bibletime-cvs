3363b0a2de164c691d36053053a825fbc28865e0
mgruner <mgruner>
2002-10-04 20:22:56 +0000

    *** empty log message ***

---
+++ setupwizard/remove.cpp
@@ -0,0 +1,16 @@
+/***************************************************************************
+                          remove.cpp  -  description
+                             -------------------
+    begin                : Fri Oct 4 2002
+    copyright            : (C) 2002 by The BibleTime team
+    email                : info@bibletime.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
