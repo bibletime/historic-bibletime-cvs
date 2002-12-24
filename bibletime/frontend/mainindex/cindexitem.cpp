9334742ce633df2d9e46b4b4ec4e59c79c00d1ca
joachim <joachim>
2002-12-24 16:51:36 +0000

    bookmark fixes

--- bibletime/frontend/mainindex/cindexitem.cpp
+++ bibletime/frontend/mainindex/cindexitem.cpp
@@ -1018,6 +1018,7 @@ void CBookmarkFolder::importBookmarks(){
   if (!fileName.isEmpty()) {
     //we have to decide if we should load an old bookmark file from 1.2 or earlier or the new XML format of > 1.3    
     if ( !loadBookmarks(fileName) ) { //if this fialed try to load it as old bookmark file
+      qWarning("use OldBookmarkImport");
       OldBookmarkImport import;
       loadBookmarksFromXML( import.oldBookmarksXML( fileName ) );
     };
@@ -1133,7 +1134,7 @@ const bool CBookmarkFolder::loadBookmarks( const QString& filename ){
 		file.close();	
 	}
 
-  loadBookmarksFromXML( xml );
+  return loadBookmarksFromXML( xml );
 }
 
 /* NEW CLASS */
