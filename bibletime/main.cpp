8a76b8eed60d2d6cfcae1251a4aedde552c77efc
mgruner <mgruner>
2003-01-18 21:05:29 +0000

    updated Bob Harman's email address according to the information from Fred Saalbach.

--- bibletime/main.cpp
+++ bibletime/main.cpp
@@ -170,7 +170,7 @@ int main(int argc, char* argv[]) {
   *        Credits (sorted by last name)         *
   ************************************************/
   // Bible study HowTo
-  aboutData.addCredit("Bob Harman",        I18N_NOOP("Bible study HowTo"), "N_Cov_Church@compuserve.com", "");
+  aboutData.addCredit("Bob Harman",        I18N_NOOP("Bible study HowTo"), "ncc@ncchampton.org", "");
   // Language codes iso639-1, iso639-2 and SIL language codes
   aboutData.addCredit("SIL International", I18N_NOOP("Language codes and names"), "", "http://www.ethnologue.com/iso639");
 
