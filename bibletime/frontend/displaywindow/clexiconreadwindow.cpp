2fbb6a07d7d1991ef8f3c14ef349e9fe66e3a2a9
joachim <joachim>
2005-08-04 14:20:13 +0000

    fixed some KDE 3.1 compat issues

--- bibletime/frontend/displaywindow/clexiconreadwindow.cpp
+++ bibletime/frontend/displaywindow/clexiconreadwindow.cpp
@@ -25,7 +25,7 @@
 #include <klocale.h>
 #include <kpopupmenu.h>
 #include <kdeversion.h>
-#include <kactionclasses.h>
+// #include <kactionclasses.h>
 
 CLexiconReadWindow::CLexiconReadWindow(ListCSwordModuleInfo moduleList, CMDIArea* parent, const char *name) : CReadWindow(moduleList, parent,name) {
 	setKey( CSwordKey::createInstance(moduleList.first()) );
